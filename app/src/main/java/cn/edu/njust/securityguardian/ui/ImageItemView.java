40b53c773caa2f33e44630bad4bd39a0868f7dab
speedcat007 <shanleizhenzhiwo@gmail.com>
2015-02-01 09:36:53 +0800

    增加不同硬件环境下的布局

---
+++ app/src/main/java/cn/edu/njust/securityguardian/ui/ImageItemView.java
@@ -0,0 +1,53 @@
+package cn.edu.njust.securityguardian.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.BitmapFactory;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import cn.edu.njust.securityguardian.R;
+
+/**
+ * Created by fookey on 15-1-31.
+ */
+public class ImageItemView extends FrameLayout {
+    private ImageButton imageButton;
+    private TextView textView;
+
+    public ImageItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.image_item,this);
+        textView=(TextView)findViewById(R.id.image_item_textview);
+        imageButton=(ImageButton)findViewById(R.id.image_item_imagebutton);
+
+        TypedArray typedArray=context.obtainStyledAttributes(attrs,R.styleable.ImageItemView);
+        int n=typedArray.getIndexCount();
+        for(int i=0;i<n;i++){
+            int attr=typedArray.getIndex(i);
+            switch (attr){
+                case R.styleable.ImageItemView_imageSrc:
+                    setImageSrc(typedArray.getResourceId(attr,0));
+                    break;
+                case R.styleable.ImageItemView_itemtext:
+                    setItemText(typedArray.getString(R.styleable.ImageItemView_itemtext));
+                    break;
+            }
+
+        }
+    }
+    public void setImageSrc(int resourceID){
+        imageButton.setImageBitmap(
+                BitmapFactory.decodeResource(getResources(), resourceID
+                ));
+    }
+    public void setItemText(String string){
+        textView.setText(string);
+    }
+    public void setOnClickListener(OnClickListener l){
+        imageButton.setOnClickListener(l);
+    }
+}
---
+++ app/src/main/java/cn/edu/njust/securityguardian/ui/TitleBackView.java
@@ -0,0 +1,54 @@
+package cn.edu.njust.securityguardian.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import cn.edu.njust.securityguardian.R;
+
+/**
+ * Created by fookey on 15-1-31.
+ */
+public class TitleBackView extends FrameLayout {
+    private Button backButton;
+    private TextView titleText;
+
+    public TitleBackView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.title_back, this);
+        titleText = (TextView) findViewById(R.id.title_text);
+        backButton = (Button) findViewById(R.id.button_back_1);
+        backButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity) getContext()).finish();
+            }
+        });
+
+        TypedArray typedArray=context.obtainStyledAttributes(attrs,R.styleable.TitleBackView);
+        int n=typedArray.getIndexCount();
+        for(int i=0;i<n;i++){
+            if(typedArray.getIndex(i)==R.styleable.TitleBackView_titleText)
+                setTitleText(typedArray.getString(R.styleable.TitleBackView_titleText));
+        }
+    }
+
+    public void setTitleText(String text) {
+        this.titleText.setText(text);
+    }
+
+    public void setLeftButtonText(String text) {
+        backButton.setText(text);
+    }
+
+    public void setBackButtonListener(OnClickListener l){
+        backButton.setOnClickListener(l);
+    }
+}
+
